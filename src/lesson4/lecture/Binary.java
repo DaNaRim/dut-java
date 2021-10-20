2a4c36a76ceeac71ca8d1505ce7512f336aed9f5
DaNaRim <rangar.danarim@gmail.com>
2021-10-21 01:01:48 +0300

    lesson4.lecture

---
+++ src/lesson4/lecture/Binary.java
@@ -0,0 +1,28 @@
+package lesson4.lecture;
+
+import java.util.Scanner;
+
+public class Binary {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        scanner.close();
+
+        int powerOfTwo = 1;
+
+        while (powerOfTwo <= n / 2) {
+            powerOfTwo *= 2;
+        }
+
+        while (powerOfTwo > 0) {
+            if (n < powerOfTwo) {
+                System.out.print(0);
+            } else {
+                System.out.print(1);
+                n -= powerOfTwo;
+            }
+            powerOfTwo /= 2;
+        }
+    }
+}
