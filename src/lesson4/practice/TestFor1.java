c8a0ed5ae5bc1a011a440e967c1b0627a7c9ba77
DaNaRim <rangar.danarim@gmail.com>
2021-10-21 01:01:04 +0300

    lesson4.practice some fixes

--- src/lesson4/practice/TestFor1.java
+++ src/lesson4/practice/TestFor1.java
@@ -3,7 +3,8 @@ package lesson4.practice;
 public class TestFor1 {
 
     public static void main(String[] args) {
-        for (int i = 0; i < 999; i++) { // 999 times
+        for (int i = 0; i < 999; i++) ; // 1 time
+        {
             System.out.println("Hello");
         }
     }
