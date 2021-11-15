9722f5fe1183a658620c6adeac40333ae0dd75bb
DaNaRim <rangar.danarim@gmail.com>
2021-11-15 12:51:16 +0200

    Add new solution variant for NPokerHands

--- src/lesson5/lecture/NPokerHands.java
+++ src/lesson5/lecture/NPokerHands.java
@@ -5,12 +5,17 @@ import java.util.Scanner;
 public class NPokerHands {
 
     public static void main(String[] args) {
-        String[] deck = {"Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"};
-        String[] suits = {"Clubs", "Diamonds", "Hearts", "Spades"};
-
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         sc.close();
+
+        variant1(n);
+//        variant2(n);
+    }
+
+    static void variant1(int n) {
+        String[] deck = {"Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"};
+        String[] suits = {"Clubs", "Diamonds", "Hearts", "Spades"};
 
         for (int i = 0; i < n; i++) {
             String[] resultDeck = new String[5];
@@ -22,11 +27,8 @@ public class NPokerHands {
                     isUnique = true;
 
                     int randNum = (int) (Math.random() * (deck.length - 1));
-                    String randNudeCard = deck[randNum];
                     int randSuits = (int) (Math.random() * (suits.length - 1));
-                    String randNudeSuits = suits[randSuits];
-
-                    randCard = randNudeSuits + randNudeCard;
+                    randCard = deck[randNum] + suits[randSuits];
 
                     for (String str : resultDeck) {
                         if (randCard.equals(str)) {
@@ -39,6 +41,37 @@ public class NPokerHands {
             }
             for (String card : resultDeck) {
                 System.out.printf("%-15s", card);
+            }
+            System.out.println();
+        }
+    }
+
+    static void variant2(int n) {
+        String[] nums = {"Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"};
+        String[] suits = {"Clubs", "Diamonds", "Hearts", "Spades"};
+
+        for (int num = 0; num < n; num++) {
+            String[] desk = new String[52];
+
+            int deskIndex = 0;
+            for (int i = 0; i < suits.length; i++) {
+                for (int j = 0; j < nums.length; j++) {
+                    desk[deskIndex] = nums[j] + suits[i];
+                    deskIndex++;
+                }
+            }
+
+            for (int i = 0; i < 50; i++) {
+                int replaceFrom = (int) (Math.random() * desk.length);
+                int replaceTo = (int) (Math.random() * desk.length);
+
+                String temp = desk[replaceTo];
+                desk[replaceTo] = desk[replaceFrom];
+                desk[replaceFrom] = temp;
+            }
+
+            for (int i = 0; i < 5; i++) {
+                System.out.printf("%-15s", desk[i]);
             }
             System.out.println();
         }
