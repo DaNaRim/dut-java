01db5ea481ae381ee4cf56b444fd82ac6f43f4b3
DaNaRim <rangar.danarim@gmail.com>
2022-02-16 14:42:40 +0200

    sem2 lesson2

---
+++ src/sem2/lesson2/SomeClass.java
@@ -0,0 +1,12 @@
+package sem2.lesson2;
+
+public class SomeClass {
+
+    public static void main(String[] args) {
+        System.out.println(sum(3, 4));
+    }
+
+    public static int sum(int a, int b) {
+        return a + b;
+    }
+}
