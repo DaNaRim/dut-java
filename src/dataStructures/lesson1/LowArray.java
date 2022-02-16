6276f135d96aff99ca0de3672882a5c0119a2daf
DaNaRim <rangar.danarim@gmail.com>
2022-02-16 14:41:11 +0200

    data structures lesson1

---
+++ src/dataStructures/lesson1/LowArray.java
@@ -0,0 +1,20 @@
+package dataStructures.lesson1;
+
+class LowArray {
+    private long[] a;
+
+    public LowArray(int size) {
+        a = new long[size];
+    }
+
+    public void setElem(int index, long value) {
+        a[index] = value;
+    }
+
+    public long getElem(int index) {
+        return a[index];
+    }
+
+}
+
+
